5d81e91703b9ad12d5e496b841f0db25e7442912
KamilPawlowski1510 <20102337@mail.wit.ie>
2023-12-02 12:40:21 +0000

    Add name and defeated field to boss and update toString function

--- src/main/kotlin/models/Boss.kt
+++ src/main/kotlin/models/Boss.kt
@@ -1,4 +1,13 @@
 package models
 
-class Boss {
+data class Boss(var name:String, var defeated:Boolean = false) {
+
+    //References for the colour:
+    //https://discuss.kotlinlang.org/t/printing-in-colors/22492
+    //https://gist.github.com/mgumiero9/665ab5f0e5e7e46cb049c1544a00e29f
+    override fun toString(): String {
+        val color = if (defeated) "\u001B[32m" else "\u001b[31m"
+        val reset = "\u001B[0m"
+        return "$color$name$reset"
+    }
 }
