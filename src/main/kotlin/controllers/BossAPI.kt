6381f2351c6f53e96a94c0764624dc8dc579d9ca
KamilPawlowski1510 <20102337@mail.wit.ie>
2023-12-02 14:39:22 +0000

    Add bossAPI bosses arraylist and functions to add, validateIndex, deleteBoss, toggleBoss, defeatedList and listBosses

--- src/main/kotlin/controllers/BossAPI.kt
+++ src/main/kotlin/controllers/BossAPI.kt
@@ -1,4 +1,37 @@
 package controllers
 
+import models.Boss
+import kotlin.collections.ArrayList
+
+
 class BossAPI {
+
+    private var bosses = ArrayList<Boss>()
+
+    fun add(boss: Boss): Boolean {
+        return bosses.add(boss)
+    }
+
+    private fun validateIndex(index: Int) = index > 0 && index < bosses.size
+
+    fun deleteBoss(index: Int): Boolean {
+        return if(validateIndex(index)){
+            bosses.removeAt(index)
+            true
+        } else false
+    }
+//Source: https://stackoverflow.com/questions/224311/cleanest-way-to-toggle-a-boolean-variable-in-java
+    fun toggleBoss(boss: Boss) {
+        boss.defeated = !boss.defeated
+    }
+
+    fun defeatedList(): ArrayList<String>{
+        val defeatedNames = ArrayList<String>()
+        bosses.filter { it.defeated }.forEach { defeatedNames.add(it.name) }
+        return defeatedNames
+    }
+
+    fun listBosses(): String =
+        if (bosses.isEmpty()) "No bosses stored"
+        else bosses.joinToString(separator = "\n") { "$it" }
 }
