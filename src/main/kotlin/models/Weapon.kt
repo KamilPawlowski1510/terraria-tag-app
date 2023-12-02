8bce4f040127a1ddd0488f4d7fd54121d8565aa0
KamilPawlowski1510 <20102337@mail.wit.ie>
2023-12-02 22:15:28 +0000

    Add fields to weapon, calculateDPS function and update toString

--- src/main/kotlin/models/Weapon.kt
+++ src/main/kotlin/models/Weapon.kt
@@ -1,4 +1,15 @@
 package models
 
-class Weapon {
+data class Weapon(var name: String, var damage: Int, var criticalChance: Int, var useTime: Int, var tags: ArrayList<String>, var requirements: ArrayList<String>) {
+
+    fun calculateDPS(): Int{
+        return (damage*(useTime/60))*(1+(criticalChance/100))
+    }
+
+    override fun toString(): String {
+        return """$name (${calculateDPS()})
+                  |Damage: €$damage, Use Time: $criticalChance%, Critical Strike Chance: $useTime%
+                  |""".trimMargin()
+    }
+
 }
